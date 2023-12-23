ab76da84aeb9f020d4d50a51b991b36d42d1a6d1
Chihiro Ito <chiroito107@gmail.com>
2023-12-23 22:24:53 +0900

    for Quarkus

--- ininialize.sh
+++ ininialize.sh
@@ -10,3 +10,7 @@ sudo apt install -y poppler-utils
 
 # for GraalVM Native Image
 sudo apt-get install -y build-essential libz-dev zlib1g-dev
+
+# for Quarkus
+sudo apt-get install -y openjdk-17-jre-headless maven
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
