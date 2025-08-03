2157265562798741f9cb3988c1a535c5a9472132
Chihiro Ito <chiroito107@gmail.com>
2025-08-04 07:56:44 +0900

    Update openjdk_development.sh

--- openjdk_development.sh
+++ openjdk_development.sh
@@ -36,6 +36,12 @@ cd /mnt/d/dev/jdk
 
 # for Windows
 bash configure --with-jtreg=${JTREG_DIR}/build/images/jtreg --with-boot-jdk=${LATEST_WINDOWS_JDK_DIR} --with-toolchain-version=2022
+make images CONF=windows-x86_64-server-release
 
 # for Linux
 bash configure --with-jtreg=${JTREG_DIR}/build/images/jtreg --with-boot-jdk=`sdk home java ${LATEST_LINUX_JDK}` --build=x86_64-unknown-linux-gnu --openjdk-target=x86_64-unknown-linux-gnu
+make images CONF=linux-x86_64-server-release
+
+# make test-tier1 CONF=windows-x86_64-server-release
+# make test TEST=test/jdk/jdk/jfr/XXX.java CONF=windows-x86_64-server-release
+# make test TEST=test/jdk/jdk/jfr/XXX.java JTREG="TIMEOUT_FACTOR=0.1" CONF=windows-x86_64-server-release
